f466ff4bd7f74375b8739a378b5d0ae72438b50e
eddyzow <45926119+eddyzow@users.noreply.github.com>
2025-05-03 17:40:16 -0400

    Cube Wars INDEV 25.18.02
    Optimizations to the preload screen

--- docs/main.js
+++ docs/main.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
     $("#preload").hide();
     $("#game-container").show();
   });
+  document.documentElement.scrollTop = 0;
+  $("#logo").css("opacity", "1");
+  $("#login-box").css("opacity", "1");
+  $("#login-box").css("transform", "scale(1, 1)");
 });
