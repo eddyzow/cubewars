6be451448dc3458c5e7845d4db3a7fdae1cfb9a8
eddyzow <45926119+eddyzow@users.noreply.github.com>
2025-05-03 22:39:43 -0400

    Cube Wars INDEV 25.18.03
    New changes to the signup flow, artistic changes, updates to preload, about buttons

--- docs/main.js
+++ docs/main.js
@@ -1,10 +1,40 @@
 $(document).ready(function () {
-  $("#start-btn").on("click", function () {
-    $("#preload").hide();
-    $("#game-container").show();
-  });
   document.documentElement.scrollTop = 0;
   $("#logo").css("opacity", "1");
-  $("#login-box").css("opacity", "1");
-  $("#login-box").css("transform", "scale(1, 1)");
+
+  const hasUserToken = true; // 🔄 Replace this later with your actual logic
+
+  if (hasUserToken) {
+    showBox("login-box");
+  } else {
+    showBox("register-box");
+  }
+
+  $("#start-btn").on("click", function () {});
+
+  $("#continue-btn").on("click", function () {
+    // Check validity of username. If exists:
+    hideBox("register-box");
+    showBox("register-step2-box");
+    // If does not exist:
+  });
+  $("#switch-user-btn").on("click", function () {
+    hideBox("login-box");
+    showBox("register-box");
+  });
+  $("#back-reg1").on("click", function () {
+    hideBox("register-step2-box");
+    showBox("register-box");
+  });
 });
+
+function hideBox(name) {
+  $("#" + name).css("opacity", "0");
+  $("#" + name).css("visibility", "hidden");
+  $("#" + name).css("transform", "scale(0)");
+}
+function showBox(name) {
+  $("#" + name).css("opacity", "1");
+  $("#" + name).css("visibility", "visible");
+  $("#" + name).css("transform", "scale(1)");
+}
